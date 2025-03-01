d41eb482ab08e3adde0accaaf9be7a7f8dcabf24
yassineBenabda <benabdayassine395@gmail.com>
2025-03-01 10:35:48 +0100

    first commit

--- src/main/java/dao/IGameDaoImpl.java
+++ src/main/java/dao/IGameDaoImpl.java
@@ -7,97 +7,52 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 import store.entities.Game;
+import util.JPAutil;
 
 public class IGameDaoImpl implements IGameDao {
 
+	private EntityManager entityManager = JPAutil.getEntityManager("GAME5");
+
 	@Override
 	public Game save(Game g) {
-		Connection conn = SingletonConnection.getConnection();
-		try {
-			PreparedStatement ps = conn.prepareStatement("INSERT INTO GAMES(NOM_GAME,PRIX) VALUES(?,?)");
-			ps.setString(1, g.getNomGame());
-			ps.setDouble(2, g.getPrix());
-			ps.executeUpdate();
-			PreparedStatement ps2 = conn.prepareStatement("SELECT MAX(ID_GAME) as MAX_ID FROM GAMES");
-			ResultSet rs = ps2.executeQuery();
-			if (rs.next()) {
-				g.setIdGame(rs.getLong("MAX_ID"));
-			}
-			ps.close();
-			ps2.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		EntityTransaction tx = entityManager.getTransaction();
+		tx.begin();
+		entityManager.persist(g);
+		tx.commit();
 		return g;
 	}
 
 	@Override
 	public List<Game> gamesParMC(String mc) {
-		List<Game> gas = new ArrayList<Game>();
-		Connection conn = SingletonConnection.getConnection();
-		try {
-			PreparedStatement ps = conn.prepareStatement("select * from GAMES where NOM_GAME LIKE ?");
-			ps.setString(1, "%" + mc + "%");
-			ResultSet rs = ps.executeQuery();
-			while (rs.next()) {
-				Game g = new Game();
-				g.setIdGame(rs.getLong("ID_GAME"));
-				g.setNomGame(rs.getString("NOM_GAME"));
-				g.setPrix(rs.getDouble("PRIX"));
-				gas.add(g);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		List<Game> gas = entityManager.createQuery("select g from Game g where g.nomGame like :mc").setParameter("mc", "%" + mc + "%").getResultList();
 		return gas;
 	}
 
 	@Override
 	public Game getGame(Long id) {
-		Connection conn = SingletonConnection.getConnection();
-		Game g = new Game();
-		try {
-			PreparedStatement ps = conn.prepareStatement("select * from GAMES where ID_GAME = ?");
-			ps.setLong(1, id);
-			ResultSet rs = ps.executeQuery();
-			if (rs.next()) {
-				g.setIdGame(rs.getLong("ID_GAME"));
-				g.setNomGame(rs.getString("NOM_GAME"));
-				g.setPrix(rs.getDouble("PRIX"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return g;
+		return entityManager.find(Game.class, id);
+
 	}
 
 	@Override
 	public Game updateGame(Game g) {
-		Connection conn = SingletonConnection.getConnection();
-		try {
-			PreparedStatement ps = conn.prepareStatement("UPDATE GAMES SET NOM_GAME=?,PRIX=? WHERE ID_GAME=?");
-			ps.setString(1, g.getNomGame());
-			ps.setDouble(2, g.getPrix());
-			ps.setLong(3, g.getIdGame());
-			ps.executeUpdate();
-			ps.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		EntityTransaction tx = entityManager.getTransaction();
+		tx.begin();
+		entityManager.merge(g);
+		tx.commit();
 		return g;
+
 	}
 
 	@Override
 	public void deleteGame(Long id) {
-		Connection conn = SingletonConnection.getConnection();
-		try {
-			PreparedStatement ps = conn.prepareStatement("DELETE FROM GAMES WHERE ID_GAME = ?");
-			ps.setLong(1, id);
-			ps.executeUpdate();
-			ps.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		Game game = entityManager.find(Game.class, id);
+		entityManager.getTransaction().begin();
+		entityManager.remove(game);
+		entityManager.getTransaction().commit();
 	}
 }
--- src/main/java/store/entities/Game.java
+++ src/main/java/store/entities/Game.java
@@ -2,8 +2,23 @@ package store.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "GAMES")
 public class Game implements Serializable {
+	
+	@Id
+	@Column(name = "ID_GAME")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idGame;
+	
+	@Column(name = "NOM_GAME")
 	private String nomGame;
 	private double prix;
 
